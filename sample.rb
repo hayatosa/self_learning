9964848c4eed5ba36933a420825e6ba98f846caf
hayato sato <hayatosato@hayato-MacBook-Pro.local>
2019-02-02 22:36:43 +0900

    method

--- sample.rb
+++ sample.rb
@@ -1,2 +1,23 @@
-puts "こんにちは"
-puts "nice to meet you"
+Gitの大まかな流れ
+branch作成、コードを書き進める init
+masterにbranchをマージ（branchはmasterでgit merge branch名）
+masterが最新状態になる
+github（リモートリポジトリ）にpushする
+urlがリモートとローカルを関連付けに必要
+git push origin master
+githubに反映される
+
+githubからローカルへクローンを作って作業
+git clone url
+
+branch作成し作業する
+commitまで終わったらbranchをgithubにpushすると
+githubでもbranchがmaster以外に、
+pushしたbranchが更新される
+github上でbranchをmasterにマージしてください
+とプルリクエストする
+承認されるとマージされる
+
+githubのマージされたコードをローカルへクローンする
+ローカルbranchはmasterにしておく
+git pull otigin master
